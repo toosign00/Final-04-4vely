74c2d28579ba1ad210820bc4e8b93ed83f6d4437
toosign <kevinsoo1014@gmail.com>
2025-07-29 15:16:27 +0900

    🔧 fix: lint error fix
    Close #70

--- src/app/my-page/order-history/_components/ReviewForm.tsx
+++ src/app/my-page/order-history/_components/ReviewForm.tsx
@@ -83,7 +83,7 @@ export default function ReviewForm({ productId, orderId, onSuccess, products, re
                   onClick={() => !isReviewed && onProductSelect?.(product)}
                 >
                   <div className='h-10 w-10 flex-shrink-0 overflow-hidden rounded-lg border border-gray-200 bg-gray-100'>
-                    <Image src={product.imageUrl} alt={product.name} width={40} height={40} className='h-full w-full object-cover' />
+                    <Image src={product.image} alt={product.name} width={40} height={40} className='h-full w-full object-cover' />
                   </div>
                   <div className='min-w-0 flex-1'>
                     <h4 className='text-secondary t-small line-clamp-1 font-medium'>{product.name}</h4>

05fca55ccecd27ad596963770d554524a275487c
jeong0403 <seehjl@gmail.com>
2025-08-01 03:34:35 +0900

    🛠 rename: modified file & folder path

--- src/components/_home/green-magazine/GreenMagazineSection.tsx
+++ src/components/_home/green-magazine/GreenMagazineSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { MagazinePostData } from '@/app/green-magazine/_types/magazine.types';
-import { GreenMagazineCarousel } from '@/components/_home/GreenMagazineCarousel';
+import { GreenMagazineCarousel } from '@/components/_home/green-magazine/GreenMagazineCarousel';
 import { magazineForHome } from '@/lib/functions/home/magazineForHome';
 import { useEffect, useState } from 'react';
 
